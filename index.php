cfe88c7a42025bba4083c53612e9f2566266ac55
Arjan de Ruijter <rra.am1x.ionic.blok4@gmail.com>
2019-06-26 07:07:15 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="nl-NL">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <title>Proeftoets - AM1A</title>
+    </head>
+    <body>
+      <div class="container-fluid">
+        <h1>Toets - AM1C</h1>
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">llnr</th>
+                    <th scope="col">Voornaam</th>
+                    <th scope="col">Githubnaam</th>
+                    <th scope="col">Avatar (40x40)</th>
+                    <th scope="col">link</th>
+                    <th scope="col">Cijfer voor pagina</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">19686</th>
+                    <td>Arjan</td>
+                    <td>rra-am1c-2018</td>
+                    <td><img src="./img/broc.jpg" width="40px" height="40px" /></td>
+                    <td><a href="./homepages/arjan_rra-am1c-2018.php">mijn homepage</a></td>
+                    <td>10+</td>
+                </tr>
+            </tbody>
+        </table>
+      </div>
+      <!-- Optional JavaScript -->
+      <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </body>
+</html>
