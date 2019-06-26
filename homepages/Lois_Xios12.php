ed658e9b04b45d4d7e273ed253bcb6cb733e1fe6
Xios12 <318144@student.mboutrecht.nl>
2019-06-26 14:02:05 +0200

    index.php aangepast en een buttoh toegevoegd

--- homepages/Lois_Xios12.php
+++ homepages/Lois_Xios12.php
@@ -29,14 +29,14 @@
                     <td>lois</td>
                     <td>Xios12</td>
                     <td>web</td>
-                    <td><img src="./img/lois.jpg" width="40px" height="40px" /></td>
-                    <td>10+</td>
+                    <td><img src="../img/lois.jpg" width="40px" height="40px" /></td>
+                    <td>8,687</td>
                 </tr>
             </tbody>
         </table>
       </div>
 
-      <button href="index.php" type="button" class="btn btn-primary btn-lg btn-block">terug naar index</button>
+      <th><a href="../index.php"><button class="btn btn-success btn-block" >Terug naar index</button></a></th>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
