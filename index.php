6fe0abd36047c1ef81e7e5a23c1f2f77fc6da589
Sean Kelly <seankelly@hotmail.nl>
2019-06-26 13:45:28 +0200

    Nieuwe branch, gefixt verkeerde dimensies van Tijn table en consistency toegevoegd

--- index.php
+++ index.php
@@ -1,124 +1,124 @@
 <!DOCTYPE html>
 <html lang="nl-NL">
-    <head>
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <title>Proeftoets - AM1A</title>
-    </head>
-    <body>
-      <div class="container-fluid">
-        <h1>Toets - AM1C</h1>
-        <table class="table table-hover">
-            <thead>
-                <tr>
-                    <th scope="col">llnr</th>
-                    <th scope="col">Voornaam</th>
-                    <th scope="col">Githubnaam</th>
-                    <th scope="col">Avatar (40x40)</th>
-                    <th scope="col">link</th>
-                    <th scope="col">Cijfer voor pagina</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row">19686</th>
-                    <td>Arjan</td>
-                    <td>rra-am1c-2018</td>
-                    <td><img src="./img/broc.jpg" width="40px" height="40px" /></td>
-                    <td><a href="./homepages/arjan_rra-am1c-2018.php">mijn homepage</a></td>
-                    <td>10+</td>
-                </tr>
-                <tr>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Proeftoets - AM1A</title>
+</head>
+<body>
+  <div class="container-fluid">
+    <h1>Toets - AM1C</h1>
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th scope="col">llnr</th>
+                <th scope="col">Voornaam</th>
+                <th scope="col">Githubnaam</th>
+                <th scope="col">Avatar (40x40)</th>
+                <th scope="col">link</th>
+                <th scope="col">Cijfer voor pagina</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">19686</th>
+                <td>Arjan</td>
+                <td>rra-am1c-2018</td>
+                <td><img src="./img/broc.jpg" width="40px" height="40px" /></td>
+                <td><a href="./homepages/arjan_rra-am1c-2018.php">mijn homepage</a></td>
+                <td>10+</td>
+            </tr>
+            <tr>
                 <th scope="row">320625</th>
-                    <td>Tijn</td>
-                    <td>tijn27</td>
-                    <td><img src="../img/tijn.png" alt="avatar" style="height: 30px;"></td>
-                    <td><a href="./homepages/tijn_tijn27.php">mijn homepage</a></td>
-                    <td>9</td>
-                </tr>
-                <tr>
-                    <th scope="row">324337</th>
-                    <td>Dennis</td>
-                    <td>oneandonlydennis</td>
-                    <td><img src="./img/etika.png" width="40px" height="40px" /></td>
-                    <td><a href="./homepages/dennis_oneandonlydennis.php">mijn homepage</a></td>
-                    <td>10 (duh)</td>
-                </tr>
+                <td>Tijn</td>
+                <td>tijn27</td>
+                <td><img src="../img/tijn.png" width="40px" height="40px"></td>
+                <td><a href="./homepages/tijn_tijn27.php">mijn homepage</a></td>
+                <td>9</td>
+            </tr>
+            <tr>
+                <th scope="row">324337</th>
+                <td>Dennis</td>
+                <td>oneandonlydennis</td>
+                <td><img src="./img/etika.png" width="40px" height="40px" /></td>
+                <td><a href="./homepages/dennis_oneandonlydennis.php">mijn homepage</a></td>
+                <td>10 (duh)</td>
+            </tr>
 
-                <tr>
-                    <th scope="row">320711</th>
-                    <td>Emma</td>
-                    <td>emmaX2</td>
-                    <td><img src="./img/kaas.jpeg" width="40px" height="40px" /></td>
-                    <td><a href="./homepages/emma_emmaX2.php">mijn homepage</a></td>
-                    <td>11</td>
-                </tr>
+            <tr>
+                <th scope="row">320711</th>
+                <td>Emma</td>
+                <td>emmaX2</td>
+                <td><img src="./img/kaas.jpeg" width="40px" height="40px" /></td>
+                <td><a href="./homepages/emma_emmaX2.php">mijn homepage</a></td>
+                <td>11</td>
+            </tr>
 
-                <tr>
-                    <th scope="row">323806</th>
-                    <td>Nick</td>
-                    <td>DankassGamer69</td>
-                    <td><img src="./img/Dkirby.PNG" width="40px" height="40px" /></td>
-                    <td><a href="./homepages/Nick_DankassGamer69.php">mijn homepage</a></td>
-                    <td>69 lmao</td>
-                </tr>
+            <tr>
+                <th scope="row">323806</th>
+                <td>Nick</td>
+                <td>DankassGamer69</td>
+                <td><img src="./img/Dkirby.PNG" width="40px" height="40px" /></td>
+                <td><a href="./homepages/Nick_DankassGamer69.php">mijn homepage</a></td>
+                <td>69 lmao</td>
+            </tr>
 
-                <tr>
-                    <th scope="row">322632</th>
-                    <td>Sven</td>
-                    <td>svenbroekhof</td>
-                    <td><img src="./img/sven.png" width="40px" height="40px" /></td>
-                    <td><a href="./homepages/sven_svenbroekhof.php">mijn homepage</a></td>
-                    <td>10</td>
-                </tr>
-                
-                <tr>
-                    <th scope="row">314181</th>
-                    <td>Sean Kelly</td>
-                    <td>daredevil28</td>
-                    <td><img src="./img/daredevil28.png" width="40px" height="40px" /></td>
-                    <td><a href="./homepages/SeanKelly_daredevil28">mijn homepage</a></td>
-                    <td>at least a 7</td>
+            <tr>
+                <th scope="row">322632</th>
+                <td>Sven</td>
+                <td>svenbroekhof</td>
+                <td><img src="./img/sven.png" width="40px" height="40px" /></td>
+                <td><a href="./homepages/sven_svenbroekhof.php">mijn homepage</a></td>
+                <td>10</td>
+            </tr>
 
-                </tr>
-                
-                <tr>
-                    <th scope="row">322401</th>
-                    <td>Jesse</td>
-                    <td>JSaurusRex</td>
-                    <td><img src="./img/Jesse_JSaurusRex.png" width="40px" height="40px" /></td>
-                    <td><a href="./homepages/jesse_JSaurusRex.php">mijn homepage</a></td>
-                    <td>8+-+</td>
-                </tr>
+            <tr>
+                <th scope="row">314181</th>
+                <td>Sean Kelly</td>
+                <td>daredevil28</td>
+                <td><img src="./img/daredevil28.png" width="40px" height="40px" /></td>
+                <td><a href="./homepages/SeanKelly_daredevil28">mijn homepage</a></td>
+                <td>at least a 7</td>
 
-                <tr>
-                    <th scope="row">322522</th>
-                    <td>Mats</td>
-                    <td>MatsStalenhoef</td>
-                    <td><img src="./img/mats.png" width="40px" height="40px" /></td>
-                    <td><a href="./homepages/mats_MatsStalenhoef.php">mijn homepage</a></td>
-                    <td>Weet ik veel</td>
-                </tr>
-		
-		<tr>
-                    <th scope="row">320526</th>
-                    <td>Yassine</td>
-                    <td>El Fahmi</td>
-                    <td><img src="./img/yassine.png" width="40px" height="40px" /></td>
-                    <td><a href="./homepages/yassine_yassinefahmi.php">mijn homepage</a></td>
-                    <td>10? O wacht..</td>
-                </tr>
+            </tr>
 
-            </tbody>
-        </table>
-      </div>
-      <!-- Optional JavaScript -->
-      <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    </body>
+            <tr>
+                <th scope="row">322401</th>
+                <td>Jesse</td>
+                <td>JSaurusRex</td>
+                <td><img src="./img/Jesse_JSaurusRex.png" width="40px" height="40px" /></td>
+                <td><a href="./homepages/jesse_JSaurusRex.php">mijn homepage</a></td>
+                <td>8+-+</td>
+            </tr>
+
+            <tr>
+                <th scope="row">322522</th>
+                <td>Mats</td>
+                <td>MatsStalenhoef</td>
+                <td><img src="./img/mats.png" width="40px" height="40px" /></td>
+                <td><a href="./homepages/mats_MatsStalenhoef.php">mijn homepage</a></td>
+                <td>Weet ik veel</td>
+            </tr>
+
+            <tr>
+                <th scope="row">320526</th>
+                <td>Yassine</td>
+                <td>El Fahmi</td>
+                <td><img src="./img/yassine.png" width="40px" height="40px" /></td>
+                <td><a href="./homepages/yassine_yassinefahmi.php">mijn homepage</a></td>
+                <td>10? O wacht..</td>
+            </tr>
+
+        </tbody>
+    </table>
+</div>
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
 </html>
